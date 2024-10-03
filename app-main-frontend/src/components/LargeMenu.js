6e18442905c28fd436cf7560b2eab65da0d1db5f
aarushi12321 <sethi.aarushi30@gmail.com>
2024-10-03 18:39:57 -0500

    merging new buttons

--- app-main-frontend/src/components/LargeMenu.js
+++ app-main-frontend/src/components/LargeMenu.js
@@ -1,8 +1,8 @@
 export function LargeMenu({ isExpanded }) {
   return (
-    <div className={`large-menu ${isExpanded ? "expanded" : ""}`}>
+    <div className={`large-menu ${isExpanded ? "expanded-history" : ""}`}>
       {/* Large menu content */}
-      <ul className={`${isExpanded ? "expanded" : ""}`}>
+      <ul className={`${isExpanded ? "expanded-history" : ""}`}>
         <li>Large Menu Item 1</li>
         <li>Large Menu Item 2</li>
         <li>Large Menu Item 3</li>
