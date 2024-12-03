a78dab4afb8f0599e84b0f42c5b8935307531841
aarushi12321 <sethi.aarushi30@gmail.com>
2024-12-03 15:39:19 -0600

    loading summary ...

--- app-main-frontend/src/components/SummaryFeature/summaryBox.js
+++ app-main-frontend/src/components/SummaryFeature/summaryBox.js
@@ -160,18 +160,24 @@ export function SummaryBox({ isSummaryFeature, toggleSummaryFeatureState }) {
         <FontAwesomeIcon icon={faClose} />
       </button>
       <div className="summary-card-outer-box">
-        {cardsData.map((card) => (
-          <SummaryCard
-            key={card.id}
-            title={card.title}
-            content={card.content}
-            isExpanded={expandedCard === card.id}
-            onExpand={() => handleCardClick(card.id)}
-            onClose={() => handleCardClick(card.id)}
-            wordCloud={card.wordCloud}
-            noExpanded={noExpanded}
-          />
-        ))}
+        {cardsData.length > 0 ? (
+          cardsData.map((card) => (
+            <SummaryCard
+              key={card.id}
+              title={card.title}
+              content={card.content}
+              isExpanded={expandedCard === card.id}
+              onExpand={() => handleCardClick(card.id)}
+              onClose={() => handleCardClick(card.id)}
+              wordCloud={card.wordCloud}
+              noExpanded={noExpanded}
+            />
+          ))
+        ) : (
+          <div className="loading-card">
+            <p>Loading summary...</p>
+          </div>
+        )}
       </div>
     </div>
   );
